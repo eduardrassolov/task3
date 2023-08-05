14f1d254db6d591fcbb302b887d2e4682e56c82b
eduardrassolov <eduard.rassolov@outlook.com>
2023-08-05 17:50:52 +0300

    added features get all notes and get note by ID

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,8 +1,22 @@
-export function getAllNotes() {
-  return;
+import db from "../db.json";
+import { INote } from "../interfaces/INote";
+
+export async function getAllNotes() {
+  try {
+    const notes: Array<INote> = db;
+    return notes;
+  } catch (error) {
+    throw error;
+  }
 }
-export function getNoteById(id: string) {
-  return;
+export async function getNoteById(id: string) {
+  try {
+    const note: INote | undefined = db.find((note) => note.id === id);
+    if (!note) throw new Error("Note not found, wrong id");
+    return note;
+  } catch (error) {
+    throw error;
+  }
 }
 export function createNote(data: any) {
   return;
